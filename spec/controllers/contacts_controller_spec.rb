83ada99679d91447df70a1761a1b390464a247c1
Samuel Prado <darkms@gmail.com>
2017-04-02 20:20:59 -0300

    WIP - fixing broken specs

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
+  let(:organization) { create(:organization) }
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get :index, params: {organization_id: organization.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: {organization_id: organization.id, id: 1}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #create" do
     it "returns http success" do
-      post :create
+      post :create, params: {organization_id: organization.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #update" do
     it "returns http success" do
-      put :update
+      put :update, params: {organization_id: organization.id, id: 1}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #destroy" do
     it "returns http success" do
-      delete :destroy
+      delete :destroy, params: {organization_id: organization.id, id: 1}
       expect(response).to have_http_status(:success)
     end
   end
